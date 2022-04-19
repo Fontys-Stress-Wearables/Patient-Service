3c9529d08996c3f9b89b60872e63dbf1010e046b
Zon,Dirk D.M.P.W. van <dirk.vanzon@student.fontys.nl>
2022-04-19 14:14:03 +0200

    added xunit and simple test

---
+++ Patient-Service/tests/PatientProfileTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace Patient_Service.tests;
+
+public class PatientProfileTests {
+    [Fact]
+    public void map() {
+        //Arrange
+        
+        //Act
+        
+        //Assert
+        Assert.Equal(1,1);
+    }
+}
