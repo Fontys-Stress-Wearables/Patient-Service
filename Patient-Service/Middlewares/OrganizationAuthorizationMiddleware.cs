bdee32c3fe3d102870df339158754133d8b02b7b
Rens-Br <rensbrekel@gmail.com>
2022-04-20 15:50:14 +0200

    Move org authorization to middleware from controller

---
+++ Patient-Service/Middlewares/OrganizationAuthorizationMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Identity.Web;
+using Patient_Service.Exceptions;
+using Patient_Service.Interfaces;
+
+namespace Patient_Service.Middlewares;
+
+public class OrganizationAuthorizationMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public OrganizationAuthorizationMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context, IOrganizationService organizationService)    
+    {
+        var tenant = context.User.GetTenantId();
+
+        if (tenant == null)
+        {
+            throw new NotFoundException("User has no tenantId");
+        }
+
+        if (!organizationService.Exists(tenant))
+        {
+            throw new UnauthorizedException($"tenant not found with the ID: {tenant}");
+        }
+
+        await _next(context);
+    }
+}
+
+public static class OrganizationAuthorizationMiddlewareExtensions
+{
+    public static IApplicationBuilder UseOrganizationAuthorizationMiddleware(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<OrganizationAuthorizationMiddleware>();
+    }
+}
