628b14e3b69aba43934f475397107341e266ea08
Zon,Dirk D.M.P.W. van <dirk.vanzon@student.fontys.nl>
2022-04-26 14:51:07 +0200

    Created more patient service tests

--- Patient-Service-Tests/Services/PatientServiceTest.cs
+++ Patient-Service-Tests/Services/PatientServiceTest.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Moq;
-using Patient_Service.Dtos;
+using Patient_Service.Exceptions;
 using Patient_Service.Interfaces;
 using Patient_Service.Models;
 using Patient_Service.Services;
@@ -20,6 +20,7 @@ public class PatientServiceTest
         
         _unitOfWorkMock.Setup(x => x.Patients.Add(patient)).Returns(patient);
         _unitOfWorkMock.Setup(x => x.Complete()).Returns(0);
+        _unitOfWorkMock.Setup(x => x.Patients.GetByIdAndTenant("tenant", "patient")).Returns(() => null);
 
         _natsServiceMock.Setup(x => x.Publish("", patient));
     }
@@ -30,10 +31,62 @@ public class PatientServiceTest
         //Arrange
         IPatientService patientService = new PatientService(_unitOfWorkMock.Object, _natsServiceMock.Object);
         //Act
-        var patient = patientService.CreatePatient("id", "John", "Doe", Convert.ToDateTime("Wed Oct 21, 2015"));
+        var patient = patientService.CreatePatient("id", "John", "Doe", Convert.ToDateTime("Oct 21, 2015"));
         //Assert
         Assert.Equal(patient.FirstName, "John");
         Assert.Equal(patient.LastName, "Doe");
-        Assert.Equal(patient.Birthdate, Convert.ToDateTime("Wed Oct 21, 2015"));
+        Assert.Equal(patient.Birthdate, Convert.ToDateTime("Oct 21, 2015"));
+    }
+    
+    [Fact]
+    public void CreatePatientWithoutFirstName_ShouldFail()
+    {
+        //Arrange
+        IPatientService patientService = new PatientService(_unitOfWorkMock.Object, _natsServiceMock.Object);
+        //Act
+        var exception = Assert.Throws<BadRequestException>(() => 
+            patientService.CreatePatient("id", "", "Doe", Convert.ToDateTime("Oct 21, 2015"))
+            );
+        //Assert
+        Assert.Equal("First name cannot be empty.", exception.Message);
+    }
+    
+    [Fact]
+    public void CreatePatientWithoutLastName_ShouldFail()
+    {
+        //Arrange
+        IPatientService patientService = new PatientService(_unitOfWorkMock.Object, _natsServiceMock.Object);
+        //Act
+        var exception = Assert.Throws<BadRequestException>(() => 
+            patientService.CreatePatient("id", "John", "", Convert.ToDateTime("Oct 21, 2015"))
+        );
+        //Assert
+        Assert.Equal("Last name cannot be empty.", exception.Message);
+    }
+    
+    [Fact]
+    public void CreatePatientWithDateGreaterThanCurrent_ShouldFail()
+    {
+        //Arrange
+        IPatientService patientService = new PatientService(_unitOfWorkMock.Object, _natsServiceMock.Object);
+        //Act
+        var exception = Assert.Throws<BadRequestException>(() => 
+            patientService.CreatePatient("id", "John", "Doe", Convert.ToDateTime("Dec 12, 9999"))
+        );
+        //Assert
+        Assert.Equal("Birthdate cannot be after the current date.", exception.Message);
+    }
+
+    [Fact]
+    public void GetPatient_ShouldFail()
+    {
+        //Arrange
+        IPatientService patientService = new PatientService(_unitOfWorkMock.Object, _natsServiceMock.Object);
+        //Act
+        var exception = Assert.Throws<NotFoundException>(() => 
+            patientService.GetPatient("id", "id")
+        );
+        //Assert
+        Assert.Equal("Patient with id 'id' doesn't exist.", exception.Message);
     }
 }
