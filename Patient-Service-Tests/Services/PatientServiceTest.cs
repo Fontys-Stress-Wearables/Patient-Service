e8e91aeb44fe48d4a667ff0bb67e42c92697ddd3
Zon,Dirk D.M.P.W. van <dirk.vanzon@student.fontys.nl>
2022-04-26 14:26:33 +0200

    added patient service tests

---
+++ Patient-Service-Tests/Services/PatientServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Moq;
+using Patient_Service.Dtos;
+using Patient_Service.Interfaces;
+using Patient_Service.Models;
+using Patient_Service.Services;
+using Xunit;
+
+namespace Patient_Service_Tests.Services;
+
+public class PatientServiceTest
+{
+    
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
+    private readonly Mock<INatsService> _natsServiceMock = new();
+
+    public PatientServiceTest()
+    {
+        var patient = new Patient();
+        
+        _unitOfWorkMock.Setup(x => x.Patients.Add(patient)).Returns(patient);
+        _unitOfWorkMock.Setup(x => x.Complete()).Returns(0);
+
+        _natsServiceMock.Setup(x => x.Publish("", patient));
+    }
+
+    [Fact]
+    public void CreatePatient_ShouldSucceed()
+    {
+        //Arrange
+        IPatientService patientService = new PatientService(_unitOfWorkMock.Object, _natsServiceMock.Object);
+        //Act
+        var patient = patientService.CreatePatient("id", "John", "Doe", Convert.ToDateTime("Wed Oct 21, 2015"));
+        //Assert
+        Assert.Equal(patient.FirstName, "John");
+        Assert.Equal(patient.LastName, "Doe");
+        Assert.Equal(patient.Birthdate, Convert.ToDateTime("Wed Oct 21, 2015"));
+    }
+}
