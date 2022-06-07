781f749159c6857d12a9cbdb8266edbc41f38f7f
roel <roelwalravenhome@gmail.com>
2022-06-07 11:42:09 +0200

    removed nats in controller

--- Patient-Service/Controllers/PatientController.cs
+++ Patient-Service/Controllers/PatientController.cs
@@ -17,15 +17,13 @@ public class PatientController : ControllerBase
 {
     private readonly IPatientService _patientService;
     private readonly IMapper _mapper;
-    private readonly INatsService _natsService;
 
     public PatientController
     (
-        IPatientService patientService, IMapper mapper, INatsService natsService)
+        IPatientService patientService, IMapper mapper)
     {
         _patientService = patientService;
         _mapper = mapper;
-        _natsService = natsService;
     }
     
     [HttpGet]
@@ -49,7 +47,7 @@ public class PatientController : ControllerBase
     public PatientDTO PostPatient(CreatePatientDTO patient)
     {
         var patientData = _patientService.CreatePatient(HttpContext.User.GetTenantId()!, patient.FirstName, patient.LastName, patient.Birthdate);
-        _natsService.Publish("patient-created",patientData);
+ 
         return _mapper.Map<PatientDTO>(patientData);
     }
     
@@ -57,7 +55,7 @@ public class PatientController : ControllerBase
     public PatientDTO UpdatePatient(string id, UpdatePatientDto patient)
     {
         var patientData = _patientService.UpdatePatient(HttpContext.User.GetTenantId()!, id, patient.FirstName, patient.LastName, patient.Birthdate);
-        _natsService.Publish("patient-updated",patientData);
+
         return _mapper.Map<PatientDTO>(patientData);
     }
     
